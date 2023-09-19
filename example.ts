344acf52d6df681ab26e4b656b50532c15416b9e
AE <drew@Andrews-MacBook-Pro.local>
2023-09-19 00:53:26 -0600

    first commit of tutorial up to Union Types

--- example.ts
+++ example.ts
@@ -1,8 +1,39 @@
 // Type Annotations
 
+//let myName: string  = "Andrew";
+//let isCool: boolean = true;
+//let favNum: number = 333;
+//
+//function add(a: number, b: number) {
+//    return a + b;
+//}
+//
+//console.log(add(5,5));
+
 // Inferred Types
 
+let myName: string  = "Andrew";
+let isCool: boolean = true;
+let favNum: number = 333;
+
+//myName = 300; // testing the noEmitOnError option
+//myName = 42; with a curly { brace error too
+
+function add(a: number, b: number) {
+    return a + b;
+}
+
+let result = add(5,5);
+
+console.log(result);
+
 // Union Types
+
+
+const numbersAndStrings: (string | number) = ["jay", 0, "Andrew", 42];
+
+console.log(numbersAndStrings);
+
 
 // Objects and Interfaces
 
